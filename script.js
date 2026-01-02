1d0299c75d1a6022bad97cf802ee3dbe9e002c9b
Younes Khalloufi <126407921+YounesKhalloufi@users.noreply.github.com>
2026-01-02 16:39:11 +0100

    Simplify portfolio about section

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Portfolio Data Object
 const portfolioData = {
-  about: "Hi! I'm Younes Khalloufi, a passionate software engineer with a Master's degree in Computer Science and Complex Systems Engineering, specializing in Artificial Intelligence and Robotics.",
+  about: "Hi! I’m Younes Khalloufi, a software engineer specialized in AI and Robotics.",
   email: "YounesKhalloufi98@gmail.com",
   phone: "+393505981964",
   projects: [
